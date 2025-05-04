f83eb4a5727e944b7b16a11f75b687eb1f3aa867
narendra325 <narendrasingampalli2@gmail.com>
2025-05-04 14:26:35 +0530

    Front end react added

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Or your main stylesheet
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { connectDB } from "./src/config/database.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", authRoutes);
