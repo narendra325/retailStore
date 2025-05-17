d1e1654c08b3dd10970a7740e8b4fe6c2b7b75f1
narendra325 <narendrasingampalli2@gmail.com>
2025-05-17 20:56:01 +0530

    ui changes

--- Frontend/src/components/Products/FeatureSection.jsx
+++ Frontend/src/components/Products/FeatureSection.jsx
@@ -5,22 +5,22 @@ import { BsCreditCard } from "react-icons/bs";
 
 export const FeatureSection = () => {
   return (
-    <div className="p-6">
+    <div className="py-16">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 text-center gap-6 ">
         <div className="flex flex-col items-center text-center bg-white">
           <FaShoppingBag />
-          <h2>FREE INTERNATIONAL SHIPPING</h2>
-          <p>on all orders over $100</p>
+          <h2 className="text-lg mt-3 font-medium">FREE INTERNATIONAL SHIPPING</h2>
+          <p className="text-md text-slate-500">on all orders over $100</p>
         </div>
         <div className="flex flex-col items-center text-center bg-white">
           <TbRefresh />
-          <h2>45 DAYS RETURN</h2>
-          <p>Moneyback gurentee</p>
+          <h2 className="text-lg mt-3 font-medium">45 DAYS RETURN</h2>
+          <p className="text-md text-slate-500">Money refund gurentee</p>
         </div>
         <div className="flex flex-col items-center text-center bg-white">
           <BsCreditCard />
-          <h2>SECURITY CHECKOUT</h2>
-          <p>100% secured checkout process</p>
+          <h2 className="text-lg mt-3 font-medium">SECURITY CHECKOUT</h2>
+          <p className="text-md text-slate-500">100% secured checkout process</p>
         </div>
       </div>
     </div>
